cc8532ec467a22c3ff6db657e4aaf94273c64978
Fallah Andy Prakasa <me@flla.my.id>
2022-07-24 14:06:56 +0700

    Install and Configure WindiCSS

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,9 +1,11 @@
+const WindiCSSWebpackPlugin = require("windicss-webpack-plugin");
+
 module.exports = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.target = 'electron-renderer';
+      config.target = "electron-renderer";
     }
-
+    config.plugins.push(new WindiCSSWebpackPlugin());
     return config;
   },
 };
